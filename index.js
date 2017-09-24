3243a194816b0742b6899fc0be2e4cb6390ac1b5
Shon0221 <yoy285437@gmail.com>
2017-09-24 21:01:24 +0800

    20170924
    1.  打通ajax 與nodejs之間的連接

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    connectDomain = require('connect-domain'),
+    logger = require('morgan'),
+    session = require('express-session');
+
+
+const http = express();
+const portOfHTTP = process.env.PORT || 8080;
+
+// Http Configuration
+http.use(express.static(__dirname + '/public'));
+http.use(logger('dev'));
+http.use(bodyParser.text({ defaultCharset: 'utf-8' }));
+http.use(connectDomain());
+// http.use(session({
+//     resave: false, // don't save session if unmodified
+//     saveUninitialized: false, // don't create session until something stored
+//     secret: 'shhhh, very secret'
+// }));
+
+// Added for error: request entity too large --start
+http.use(bodyParser.json({ limit: '64mb' }));
+http.use(bodyParser.urlencoded({ limit: '64mb', extended: true }));
+// Added for error: request entity too large --end
+
+http.listen(portOfHTTP, function() {});
+
+// Routes
+require('./routers/RoutersOfHttp.js')(http);
---
+++ routers/RoutersOfHttp.js
@@ -0,0 +1,31 @@
+module.exports = function(app) {
+
+    // Add headers
+    //https://stackoverflow.com/questions/18310394/no-access-control-allow-origin-node-apache-port-issue
+    app.use(function(req, res, next) {
+
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', '*');
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+
+        res.setHeader('Content-Type', 'application/json');
+        // Pass to next layer of middleware
+        next();
+    });
+
+    app.get('/GetLocation', function(req, res) {
+        console.log('node js ');
+        const msg = { 'Host': 'Ta' };
+        res.json(msg);
+        res.end();
+    });
+}
